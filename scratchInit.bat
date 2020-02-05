70c7293c293604c3d7b0b2b002abb214bcb59a20
Franck <franckdepicardie@gmail.com>
2020-02-05 17:40:06 +0100

    fix dos script

---
+++ scratchInit.bat
@@ -0,0 +1,36 @@
+REM Bash Script used for creating new scratch org 
+
+REM Prerequisites :
+REM Be sure to be placed in a sfdx project folder in order to find the correct config/project-scratch-def.json
+REM Be sure you authorized DevHub access
+
+SET YourAlias=%1
+
+ECHO "Creating New Scratch Org..."
+
+sfdx force:org:create --setdefaultusername -f config/project-scratch-def.json --setalias $YourAlias% -d 30
+
+ECHO "Starting managed package installation..."
+
+ECHO "CPQ :"
+sfdx force:package:install -p 04t4N000000YT2ZQAW -u %YourAlias%  -w 25 --noprompt
+
+ECHO "Billing :"
+sfdx force:package:install -p 04t0K00000179KyQAI -u %YourAlias%  -w 25
+
+ECHO "FA :"
+sfdx force:package:install -p 04t0M000001SbQHQA0 -u %YourAlias%  -w 25
+
+ECHO "Sparklane :"
+sfdx force:package:install -p 04t4I000000gNw8QAE -u %YourAlias%  -w 25
+
+ECHO "Conga :"
+sfdx force:package:install -p 04tf2000002FhY6AAK -u %YourAlias%  -w 25 --noprompt
+
+ECHO "Adobe Sign :"
+sfdx force:package:install -p 04t0g000000tOItAAM -u %YourAlias%  -w 25
+
+ECHO "Prodly :"
+sfdx force:package:install -p 04t0H000000p4jZQAQ -u %YourAlias%  -w 25 --noprompt
+
+ECHO "Finish installing all managed package"
--- scratchInit.sh
+++ scratchInit.sh
@@ -4,10 +4,33 @@
 # Be sure to be placed in a sfdx project folder in order to find the correct config/project-scratch-def.json
 # Be sure you authorized DevHub access
 
-YourScratchOrgUsername=$1
+YourAlias=$1
 
 echo "Creating New Scratch Org..."
 
 sfdx force:org:create --setdefaultusername -f config/project-scratch-def.json --setalias $YourAlias -d 30
 
-sfdx force:org:list 
+echo "Starting managed package installation..."
+
+echo "CPQ :"
+sfdx force:package:install -p 04t4N000000YT2ZQAW -u $YourAlias  -w 25 --noprompt
+
+echo "Billing :"
+sfdx force:package:install -p 04t0K00000179KyQAI -u $YourAlias  -w 25
+
+echo "FA :"
+sfdx force:package:install -p 04t0M000001SbQHQA0 -u $YourAlias  -w 25
+
+echo "Sparklane :"
+sfdx force:package:install -p 04t4I000000gNw8QAE -u $YourAlias  -w 25
+
+echo "Conga :"
+sfdx force:package:install -p 04tf2000002FhY6AAK -u $YourAlias  -w 25 --noprompt
+
+echo "Adobe Sign :"
+sfdx force:package:install -p 04t0g000000tOItAAM -u $YourAlias  -w 25
+
+echo "Prodly :"
+sfdx force:package:install -p 04t0H000000p4jZQAQ -u $YourAlias  -w 25 --noprompt
+
+echo "Finish installing all managed package"
