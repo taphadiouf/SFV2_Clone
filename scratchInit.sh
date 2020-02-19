6627573682547df20fb3575c24feb3bf9f98e58f
eyhuellou <60188887+eyhuellou@users.noreply.github.com>
2020-02-19 13:47:12 +0100

    Activate Sparklane Install

--- scratchInit.sh
+++ scratchInit.sh
@@ -21,8 +21,8 @@ sfdx force:package:install -p 04t0K00000179KyQAI -u $YourAlias  -w 25
 #echo "FA :"
 #sfdx force:package:install -p 04t0M000001SbQHQA0 -u $YourAlias  -w 25
 
-#echo "Sparklane :"
-#sfdx force:package:install -p 04t4I000000gNw8QAE -u $YourAlias  -w 25
+echo "Sparklane :"
+sfdx force:package:install -p 04t4I000000gNw8QAE -u $YourAlias  -w 25
 
 #echo "Conga :"
 #sfdx force:package:install -p 04tf2000002FhY6AAK -u $YourAlias  -w 25 --noprompt
