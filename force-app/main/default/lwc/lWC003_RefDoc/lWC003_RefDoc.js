380dd6cd62f53c706d31b40762d97e05e6077144
Ouarti Ayoub <ouarti.ayoub.1@gmail.com>
2020-03-03 17:47:53 +0100

    Sort ok, table mock from attachment ok, add attachment ok, connexion between the add and the table NOK

--- force-app/main/default/lwc/lWC003_RefDoc/lWC003_RefDoc.js
+++ force-app/main/default/lwc/lWC003_RefDoc/lWC003_RefDoc.js
@@ -8,17 +8,18 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
-import { LightningElement, track, api } from 'lwc';
-//import  getAccountsByRecordtype from '@salesforce/apex/APC001_IS241_LeadFaConverController.getAccountsByRecordtype';
-
+import { LightningElement, track, api,wire } from 'lwc';
+import getAllAttachments from "@salesforce/apex/APC002_AttachmentController.getAllAttachments";
+import { CurrentPageReference } from "lightning/navigation";
+//import { registerListener, unregisterAllListeners } from "c/pubsub";
 const columns = [
-    { label: 'Titre', fieldName: 'Titre' },
-    { label: 'Type', fieldName: 'type', type: 'url' },
-    { label: 'Date de création', fieldName: 'Date de création', type: 'date' }
+    { label: 'Titre', fieldName: 'Name' ,sortable: true},
+    { label: 'Type', fieldName: 'Description',sortable: true},
+    { label: 'Date de création', fieldName: 'CreatedDate', type: 'date' ,sortable: true}
 ];
 
 export default class Lwc003_RefDoc extends LightningElement {
-    @api recordId = '';
+    @api recordId;
     value;
 
     @track
@@ -27,20 +28,175 @@ export default class Lwc003_RefDoc extends LightningElement {
     @track data = [];
     @track columns = columns;
 
+    attachmentsFromDB = [];
+    @track attachments = [];
+    @track columns = columns;
+    @track errorMsg;
+    @track sortBy;
+    @track sortDirection;
+    @wire(CurrentPageReference) pageRef;
+    @track numberOfData = 10;
+    @track currentPage = 1;
+    @track numberOfAllPages;
+    paginationApex = false;
 
-    @track PopUpVisible = false;
-
-
-    constructor(){
-        super();
-       
-    }
-    async connectedCallback(){
-
-      
-     
-    }
-    
     
 
+    disconnectedCallback() {
+        // unsubscribe from bearListUpdate event
+       // unregisterAllListeners(this);
+      }
+    
+      async connectedCallback() {
+        // get All attachments from DB
+        if (!this.paginationApex) {
+          this.attachmentsFromDB = await getAllAttachments();
+        }
+        this.totalData = this.attachmentsFromDB.length;
+        this.dataTreatment(this.attachmentsFromDB);
+    
+        // Subscribe to handleOrderListResult event to receive data from The Search Component
+      //  registerListener("handleOrderListResult", this.handleOrderListResult, this);
+      }
+    
+      dataTreatment(attachments) {
+        if (this.paginationApex) {
+          getAllattachmentsWithOffset({
+            numberOfData: this.numberOfData,
+            currentPage: this.currentPage
+          }).then(result => {
+            attachments = result;
+            this.attachments = attachments;
+          });
+        }
+        if (attachments !== undefined) {
+          // reset numberOfAllPages to 1
+          if (this.numberOfData === 0 || this.numberOfData === "") {
+            this.numberOfAllPages = 1;
+          } else if (this.totalData <= this.numberOfData) {
+            // reset numberOfAllPages & currentPage to 1
+            this.numberOfAllPages = 1;
+            this.currentPage = 1;
+          } else if (this.totalData > this.numberOfData) {
+            //calculate number of all pages from the size of attachments
+            this.numberOfAllPages = Math.ceil(this.totalData / this.numberOfData);
+            this.currentPage = 1;
+          }
+        }
+    
+        if (!this.paginationApex) {
+          //get the range  of attachments from  currentPage , numberOfData
+          this.attachments = attachments.slice(
+            this.currentPage * this.numberOfData - this.numberOfData,
+            this.currentPage * this.numberOfData
+          );
+        }
+      }
+      handlNumberOfData(event) {
+        this.numberOfData = event.detail.value;
+        //Preparing data with the new value of lines to print
+        if (this.numberOfData !== "" && this.attachmentsFromDB !== undefined) {
+          this.dataTreatment(this.attachmentsFromDB);
+        }
+        if (this.numberOfData === "" || this.numberOfData === 0) {
+          this.numberOfAllPages = 1;
+          this.currentPage = 1;
+        }
+      }
+    
+      handelPrevious() {
+        if (this.currentPage !== 1) {
+          this.currentPage -= 1;
+          if (!this.paginationApex) {
+            //get the range  of attachments from  currentPage , numberOfData
+            this.attachments = this.attachmentsFromDB.slice(
+              this.currentPage * this.numberOfData - this.numberOfData,
+              this.currentPage * this.numberOfData
+            );
+          } else {
+            getAllattachmentsWithOffset({
+              numberOfData: this.numberOfData,
+              currentPage: this.currentPage
+            }).then(result => {
+              this.attachments = result;
+            });
+          }
+        }
+      }
+      handelForward() {
+        if (this.currentPage !== this.numberOfAllPages) {
+          this.currentPage += 1;
+          if (!this.paginationApex) {
+            //get the range  of attachments from  currentPage , numberOfData
+            this.attachments = this.attachmentsFromDB.slice(
+              this.currentPage * this.numberOfData - this.numberOfData,
+              this.currentPage * this.numberOfData
+            );
+          } else {
+            getAllattachmentsWithOffset({
+              numberOfData: this.numberOfData,
+              currentPage: this.currentPage
+            }).then(result => {
+              this.attachments = result;
+            });
+          }
+        }
+      }
+    
+      //Handle Data from the Search Component
+      handleOrderListResult(attachments) {
+        if (attachments === undefined) {
+          this.errorMsg = "No items to display.";
+          this.attachments = undefined;
+          this.attachmentsFromDB = undefined;
+          this.numberOfAllPages = 1;
+          this.currentPage = 1;
+        } else {
+          this.numberOfAllPages = 1;
+          this.currentPage = 1;
+          this.attachments = attachments;
+          this.errorMsg = undefined;
+          this.attachmentsFromDB = attachments;
+          this.totalData = attachments.length;
+          this.dataTreatment(attachments);
+        }
+      }
+    
+      handleSortdata(event) {
+        if (this.attachments !== undefined) {
+          // field name
+          this.sortBy = event.detail.fieldName;
+    
+          // sort direction
+          this.sortDirection = event.detail.sortDirection;
+    
+          // calling sortdata function to sort the data based on direction and selected field
+          this.sortData(event.detail.fieldName, event.detail.sortDirection);
+        }
+      }
+    
+      sortData(fieldname, direction) {
+        // serialize the data before calling sort function
+        let parseData = JSON.parse(JSON.stringify(this.attachments));
+    
+        // Return the value stored in the field
+        let keyValue = a => {
+          return a[fieldname];
+        };
+    
+        // cheking reverse direction
+        let isReverse = direction === "asc" ? 1 : -1;
+    
+        // sorting data
+        parseData.sort((x, y) => {
+          x = keyValue(x) ? keyValue(x) : ""; // handling null values
+          y = keyValue(y) ? keyValue(y) : "";
+    
+          // sorting values based on direction
+          return isReverse * ((x > y) - (y > x));
+        });
+    
+        // set the sorted data to data table data
+        this.attachments = parseData;
+      }
 }
--- force-app/main/default/lwc/lWC004_RefDocParam/lWC004_RefDocParam.js
+++ force-app/main/default/lwc/lWC004_RefDocParam/lWC004_RefDocParam.js
@@ -8,50 +8,66 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
-import { LightningElement, api,track } from "lwc";
+import { LightningElement, api, track } from "lwc";
 import save from '@salesforce/apex/APC002_AttachmentController.save';
-import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class Lwc004_RefDocParam extends LightningElement {
- 
+
   @api recordId;
-@track skip="Annuler";
-@track submit="Envoyer";
-@track uploadFile='Charger un nouveau fichier';
-@track type='Type';
-@track attachment='Fichier';
-@track uploadFiles='Upload Files';
-@track orDropFiles='or Drop Files';
-@track uploadFileHeader='Charger un fichier';
-@track picklistTypeValue = [{ label: 'PDF', value: 'PDF', selected: true },{ label: 'DOC', value: 'DOC' }];
-@track data  ={type:'',file  :File};
-    @track fileName = '';
-    @track UploadFile = 'Upload File';
-    @track showLoadingSpinner = false;
-    @track isTrue = false;
-    selectedRecords;
-    filesUploaded = [];
-    file;
-    fileContents;
-    fileReader;
-    content;
-    MAX_FILE_SIZE = 1500000;
+  @track skip = "Annuler";
+  @track submit = "Envoyer";
+  @track uploadFile = 'Charger un nouveau fichier';
+  @track type = 'Type';
+  @track attachment = 'Fichier';
+  @track uploadFiles = 'Charger un fichier';
+  @track orDropFiles = 'Ou gliser ';
+  @track uploadFileHeader = 'Charger un fichier';
+  @track picklistTypeValue = [{ label: 'Contrat Accueil', value: 'Contrat Accueil' }, 
+  { label: 'Contrat Réservataire', value: 'Contrat Réservataire' },
+  { label: 'Facture Famille', value: 'Facture Famille' }, 
+  { label: 'Facture Entreprise', value: 'CFacture Entreprise' },
+   { label: 'Pièce justificative', value: 'Pièce justificative' },
+   { label: 'Autres', value: 'Autres' }];
+  @track data = { type: '', file: File };
+  @track fileName = '';
+  @track UploadFile = 'Upload File';
+  @track showLoadingSpinner = false;
+  @track isTrue = false;
+  selectedRecords;
+  filesUploaded = [];
+  file;
+  fileContents;
+  fileReader;
+  content;
+  MAX_FILE_SIZE = 1500000;
 
 
-constructor(){
-  super();
-  if(this.picklistTypeValue === undefined || this.picklistTypeValue == null){
-    this.picklistTypeValue = [{ label: 'PDF', value: 'PDF', selected: true },{ label: 'DOC', value: 'DOC' }];
+  constructor() {
+    super();
+    if (this.picklistTypeValue === undefined || this.picklistTypeValue == null) {
+      this.picklistTypeValue = [
+        { label: 'Contrat Accueil', value: 'Contrat Accueil' }, 
+      { label: 'Contrat Réservataire', value: 'Contrat Réservataire' },
+      { label: 'Facture Famille', value: 'Facture Famille' }, 
+      { label: 'Facture Entreprise', value: 'CFacture Entreprise' },
+       { label: 'Pièce justificative', value: 'Pièce justificative' },
+       { label: 'Autres', value: 'Autres' }];
+    }
+
   }
- 
-}
-async connectedCallback(){
+  async connectedCallback() {
 
-  if(this.picklistTypeValue === undefined || this.picklistTypeValue == null){
-    this.picklistTypeValue = [{ label: 'PDF', value: 'PDF', selected: true },{ label: 'DOC', value: 'DOC' }];
+    if (this.picklistTypeValue === undefined || this.picklistTypeValue == null) {
+      this.picklistTypeValue = [{ label: 'Contrat Accueil', value: 'Contrat Accueil' }, 
+      { label: 'Contrat Réservataire', value: 'Contrat Réservataire' },
+      { label: 'Facture Famille', value: 'Facture Famille' }, 
+      { label: 'Facture Entreprise', value: 'CFacture Entreprise' },
+       { label: 'Pièce justificative', value: 'Pièce justificative' },
+       { label: 'Autres', value: 'Autres' }];
+    }
+
+
   }
- 
-
-}
 
 
 
@@ -64,7 +80,9 @@ async connectedCallback(){
 
   handleSkip() {
     this.showLoadingSpinner = false;
-
+    this.data = [];
+    this.fileName='';
+    this.filesUploaded = [];
     this.template.querySelector("section").classList.add("slds-hide");
     this.template
       .querySelector("div.modalBackdrops")
@@ -72,87 +90,91 @@ async connectedCallback(){
   }
 
 
-handleChangeFields(event) {
-  const field = event.target.name;
-  if (field === 'type') { this.data.type = event.target.value; }
+  handleChangeFields(event) {
+    const field = event.target.name;
+    if (field === 'type') { this.data.type = event.target.value; }
 
-}
+  }
 
 
- // getting file 
- handleFilesChange(event) {
-  if(event.target.files.length > 0) {
+  // getting file 
+  handleFilesChange(event) {
+    if (event.target.files.length > 0) {
       this.filesUploaded = event.target.files;
       this.fileName = event.target.files[0].name;
+    }
   }
-}
 
-handleSave() {
-  if(this.filesUploaded.length > 0) {
+  handleSave() {
+
+
+    if (this.filesUploaded.length > 0) {
       this.uploadHelper();
-  }
-  else {
+    }
+    else {
       this.fileName = 'Merci de choisir un ficher !';
+    }
   }
-}
 
-uploadHelper() {
-  this.file = this.filesUploaded[0];
- if (this.file.size > this.MAX_FILE_SIZE) {
+  uploadHelper() {
+    this.file = this.filesUploaded[0];
+    if (this.file.size > this.MAX_FILE_SIZE) {
       window.console.log('File Size is to long');
-      return ;
-  }
-  this.showLoadingSpinner = true;
-  // create a FileReader object 
-  this.fileReader= new FileReader();
-  // set onload function of FileReader object  
-  this.fileReader.onloadend = (() => {
+      return;
+    }
+    this.showLoadingSpinner = true;
+    // create a FileReader object 
+    this.fileReader = new FileReader();
+    // set onload function of FileReader object  
+    this.fileReader.onloadend = (() => {
       this.fileContents = this.fileReader.result;
       let base64 = 'base64,';
       this.content = this.fileContents.indexOf(base64) + base64.length;
       this.fileContents = this.fileContents.substring(this.content);
-      
       // call the uploadProcess method 
       this.saveToFile();
-  });
+    });
 
-  this.fileReader.readAsDataURL(this.file);
-}
+    this.fileReader.readAsDataURL(this.file);
+  }
 
-// Calling apex class to insert the file
-saveToFile() {
-  save({ description: this.data.type, idParent: this.recordId, strFileName: this.file.name, base64Data: encodeURIComponent(this.fileContents)})
-  .then(result => {
-      window.console.log('result ====> ' +result);
-      // refreshing the datatable
-      this.getRelatedFiles();
 
-      this.fileName = this.fileName + ' - Uploaded Successfully';
-      this.UploadFile = 'File Uploaded Successfully';
-      this.isTrue = true;
-      this.showLoadingSpinner = false;
 
-      // Showing Success message after file insert
-      this.dispatchEvent(
+  // Calling apex class to insert the file
+  saveToFile() {
+
+    save({ idParent: this.recordId, strFileName: this.file.name, base64Data: encodeURIComponent(this.fileContents), description: this.data.type })
+      .then(result => {
+        window.console.log('result ====> ' + result);
+        // refreshing the datatable
+
+        this.fileName = this.fileName + ' - Uploaded Successfully';
+        this.UploadFile = 'File Uploaded Successfully';
+        this.isTrue = true;
+        this.showLoadingSpinner = false;
+
+        // Showing Success message after file insert
+        this.dispatchEvent(
           new ShowToastEvent({
-              title: 'Success!!',
-              message: this.file.name + ' - Uploaded Successfully!!!',
-              variant: 'success',
+            title: 'Succès !',
+            message: this.file.name + ' - chargé avec succès !',
+            variant: 'success',
           }),
-      );
+        );
+        this.handleSkip();
 
-  })
-  .catch(error => {
-      // Showing errors if any while inserting the files
-      window.console.log(error);
-      this.dispatchEvent(
+      })
+      .catch(error => {
+        // Showing errors if any while inserting the files
+        window.console.log(error);
+        this.dispatchEvent(
           new ShowToastEvent({
-              title: 'Error while uploading File',
-              message: error.message,
-              variant: 'error',
+            title: 'Error de chargement du fichier',
+            message: error.message,
+            variant: 'error',
           }),
-      );
-  });
-}
+        );
+      });
+  }
 
 }
