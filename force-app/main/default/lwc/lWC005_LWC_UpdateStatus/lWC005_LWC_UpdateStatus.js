ae15da870ec737f9e87c42fe582f8d1eb54a9c46
hmakboub <hmakboub@salesforce.com>
2020-03-06 16:36:58 +0000

    undo the weekend commit

--- force-app/main/default/lwc/lWC005_LWC_UpdateStatus/lWC005_LWC_UpdateStatus.js
+++ force-app/main/default/lwc/lWC005_LWC_UpdateStatus/lWC005_LWC_UpdateStatus.js
@@ -2,23 +2,21 @@ import { LightningElement, wire, api,track } from 'lwc';
 import  getPicklistvalues from '@salesforce/apex/LWC003_LWC_UpdateStatusController.getPicklistvalues';
 
 
+
 export default class LWC005_LWC_UpdateStatus extends LightningElement {
     @track openmodel = false;
     value;
     options;
-    
-
 
     constructor(){
         super();
-        console.log("constructor2!")
         this.options = [];
     }
     
 
 
 
-    connectedCallback(){
+    connectedCallback(event){
 
         console.log("ConnectedCallback5!")
         
@@ -32,27 +30,32 @@ export default class LWC005_LWC_UpdateStatus extends LightningElement {
                 });
             }
             console.log(options);
+            this.value = event.detail.value;
+            console.log(this.value);
         })
         .catch(err=>{
             console.error(err);
-        });
-        
+        });               
     }
-
-
-
-
-
-
-
     openmodal() {
         this.openmodel = true
     }
     closeModal() {
         this.openmodel = false
     } 
-    updateMethod() {
-        alert('save method invoked');
-        this.closeModal();
+    updatePreinscription() {
+        console.log('Laliste modifiée');
+        updateLstPreinscription({
+            statut :this.value
+        })
+        console.log('test')
+        console.log('hanae'+this.statut)
+        .then((result)=>{
+            this.lstPreinscription = result;
+            console.log('Laliste modifiée'+result);
+        })
+        .catch(err=>{
+            console.error(err);
+        });
     }
 }
