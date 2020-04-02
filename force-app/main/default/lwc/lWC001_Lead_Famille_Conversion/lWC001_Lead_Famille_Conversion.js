4c60199d7397e7bcef081f56a373ef6372e5dad2
First Last <you@email.com>
2020-04-02 14:40:19 +0100

    First changes on LWC001_Lead_Famille_Conversion

--- force-app/main/default/lwc/lWC001_Lead_Famille_Conversion/lWC001_Lead_Famille_Conversion.js
+++ force-app/main/default/lwc/lWC001_Lead_Famille_Conversion/lWC001_Lead_Famille_Conversion.js
@@ -11,7 +11,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import  getAccountsByRecordtype from '@salesforce/apex/APC001_LeadFaConverController.getAccountsByRecordtype';
 import LPCR_PopConLT from '@salesforce/label/c.LPCR_PopConLT';
 import LPCR_PopConLT2 from '@salesforce/label/c.LPCR_PopConLT2';
@@ -21,52 +21,52 @@ import LPCR_CrecheWA from '@salesforce/label/c.LPCR_CrecheWA';
 
 
 export default class LWC001_Lead_Famille_Conversion extends LightningElement {
-    label = {
-        LPCR_PopConLT,
-        LPCR_PopConLT2,
-        LPCR_CrecheWA,
-    };
+    label; 
     @api recordId = '';
     value;
     options;
-    recType = 'Creche';
-
-    @track areDetailsVisible = false; 
-    @track DetailsVisible = false;
-
+    recType;
+    creche1Value;
+    creche2Value;
+    showCreches1;
+    showCreches2;
+    creches;
     constructor(){
         super();
         console.log("constructor2!")
         this.options = [];
+        this.label = {
+            LPCR_PopConLT,
+            LPCR_PopConLT2,
+            LPCR_CrecheWA,
+        };
+        this.recType = 'Creche';
+        this.showCreches1 = false;
+        this.showCreches2 = false;
+        this.creches = [];
     }
-    connectedCallback(){
-
-        console.log("ConnectedCallback5!")
-        
-        
-        getAccountsByRecordtype(
-            {
-                recordtypeId : this.recType
-            }
-        )
-        .then(accounts=>{
-            for(let i=0; i< accounts.length; i++){
-                this.options.push({
-                    label: accounts[i].Name,
-                    value: accounts[i].Name
+    @wire(getAccountsByRecordtype, {recordTypeName : "$recType"})
+    getAccountsByRecordtypeF({error, data}){
+        if(data){
+            for(let i=0; i< data.length; i++){
+                this.creches.push({
+                    label: data[i].Name,
+                    value: data[i].Name
                 });
             }
-        })
-        .catch(err=>{
-            console.error(err);
-        });
-        
+        }
+        else if(error){
+            console.error(error);
+        }
     }
-    
-    handleChange(event) {
-        this.areDetailsVisible = event.target.checked;
+
+    connectedCallback(){
+        console.log("ConnectedCallback11!");
     }
-    handleChange2(event) {
-        this.DetailsVisible = event.target.checked;
+    handleContrDaccCheckbox(event) {
+        this.showCreches1 = event.target.checked;
+    }
+    handleSponsCheckbox(event) {
+        this.showCreches2 = event.target.checked;
     }
 }
