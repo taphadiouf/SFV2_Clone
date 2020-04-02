56fbb88ff8a4ba12b36943076d2e4049b2623344
First Last <you@email.com>
2020-04-02 17:01:47 +0100

    Added logic to get the current lead children

--- force-app/main/default/lwc/lWC001_Lead_Famille_Conversion/lWC001_Lead_Famille_Conversion.js
+++ force-app/main/default/lwc/lWC001_Lead_Famille_Conversion/lWC001_Lead_Famille_Conversion.js
@@ -13,6 +13,7 @@
 /* eslint-disable no-alert */
 import { LightningElement, api, wire } from 'lwc';
 import  getAccountsByRecordtype from '@salesforce/apex/APC001_LeadFaConverController.getAccountsByRecordtype';
+import  getCurrentLead from '@salesforce/apex/APC001_LeadFaConverController.getCurrentLead';
 import LPCR_PopConLT from '@salesforce/label/c.LPCR_PopConLT';
 import LPCR_PopConLT2 from '@salesforce/label/c.LPCR_PopConLT2';
 import LPCR_CrecheWA from '@salesforce/label/c.LPCR_CrecheWA';
@@ -31,6 +32,7 @@ export default class LWC001_Lead_Famille_Conversion extends LightningElement {
     showCreches1;
     showCreches2;
     creches;
+    enfants;
     constructor(){
         super();
         console.log("constructor2!")
@@ -44,6 +46,7 @@ export default class LWC001_Lead_Famille_Conversion extends LightningElement {
         this.showCreches1 = false;
         this.showCreches2 = false;
         this.creches = [];
+        this.enfants = {};
     }
     @wire(getAccountsByRecordtype, {recordTypeName : "$recType"})
     getAccountsByRecordtypeF({error, data}){
@@ -59,9 +62,39 @@ export default class LWC001_Lead_Famille_Conversion extends LightningElement {
             console.error(error);
         }
     }
+    @wire(getCurrentLead, {leadId : "$recordId"})
+    getCurrentLeadF({error, data}){
+        if(data){
+            console.log(data);
+            let tempNomField;
+            let tempPrenomField;
+            for(let i = 1; i<4 ; i++){
+                tempNomField = "LPCR_NomEnfant" + i + "__c";
+                tempPrenomField = "LPCR_PrenomEnfant" + i + "__c";
+                if(data[tempNomField] && data[tempPrenomField]){
+                    this.enfants["enfant" + i] = {
+                        nom : data[tempNomField],
+                        prenom : data[tempPrenomField]
+                    };
+                }
+            }
+        }
+        else if(error){
+            console.error(error);
+        }
+    }
 
     connectedCallback(){
         console.log("ConnectedCallback11!");
+    }
+    get showEnfants(){
+        let c = 0;
+        for(let i = 1; i < 4; i++){
+            if(this.enfants["enfant" + i] != null){
+                c++;
+            }
+        }
+        return c > 1;
     }
     handleContrDaccCheckbox(event) {
         this.showCreches1 = event.target.checked;
