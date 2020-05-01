1765c7b6fee6b9ac167eff3483ddb9decd582043
eyhuellou <60188887+eyhuellou@users.noreply.github.com>
2020-05-01 15:00:36 +0200

    Update scratchInit.sh

--- scratchInit.sh
+++ scratchInit.sh
@@ -24,18 +24,18 @@ sfdx force:package:install -p 04t4I000000gNw8QAE -u $YourAlias  -w 25
 echo "----------- Adobe Sign :"
 sfdx force:package:install -p 04t0g000000tOItAAM -u $YourAlias  -w 25
 
+echo "----------- CPQ - Conga Quotes :"
+sfdx force:package:install -p 04tf20000003UBeAAM -u $YourAlias  -w 25 --noprompt
+
+echo "----------- Conga Invoices Generation :"
+sfdx force:package:install -p 04t2M000002mdK2QAI -u $YourAlias  -w 25 --noprompt
+
 #echo "----------- FA :"
 #sfdx force:package:install -p 04t0M000001SbQHQA0 -u $YourAlias  -w 25
-
-#echo "----------- Conga :"
-#sfdx force:package:install -p 04tf2000002FhY6AAK -u $YourAlias  -w 25 --noprompt
 
 #echo "----------- Prodly :"
 #sfdx force:package:install -p 04t0H000000p4jZQAQ -u $YourAlias  -w 25 --noprompt
 
 echo "Finish installing all managed package"
-
-#echo "assign LPCR Admin permission set to default scratch org user..."
-#sfdx force:user:permset:assign -n LPCR_Admin
 
 echo "scratchInit finished"
