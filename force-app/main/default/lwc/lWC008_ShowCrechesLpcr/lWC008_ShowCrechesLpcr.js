bfd3631a34f12defe6a0733e6b46bc9a2c4370d5
First Last <achraf.aaboul@gmail.com>
2020-06-04 03:28:49 +0100

    Cleaned component code

--- force-app/main/default/lwc/lWC008_ShowCrechesLpcr/lWC008_ShowCrechesLpcr.js
+++ force-app/main/default/lwc/lWC008_ShowCrechesLpcr/lWC008_ShowCrechesLpcr.js
@@ -5,36 +5,13 @@ export default class LWC008_ShowCrechesLpcr extends LightningElement {
     @api recordId;
     @api modeAffich;
     accounts;
-    center = {
-        Street: "Rue Barathon",
-        PostalCode: "20250"
-    };
-    error;
-    showFooter = true ;
-    /*
-    @wire(getAccounts, {currentId : "$recordId", modeAffich: "$modeAffich"})
-     wiredAccountsF({error,data}) {
-         if (data) {f
-             this.accounts = data;
-             console.log(data);
-             console.log(JSON.stringify(data, null, '\t'));
-         } else if (error) {
-             console.log(error);
-             this.error = error;
-         }
-     }*/
+    center;
      connectedCallback(){
-         console.log('gello');
-         console.log('recordId :' + this.recordId);
-         console.log('modAffich : ' + this.modeAffich);
         getAccounts({currentId : this.recordId, modeAffich: this.modeAffich})
         .then(res=>{
             let jsonResult = JSON.parse(res);
             this.accounts = jsonResult.mapMarkers;
             this.center = jsonResult.center;
-            console.log('hii');
-            console.log(this.accounts);
-            console.log(this.center);
         })
         .catch(err=>{
             console.log(err);
