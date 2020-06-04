eb0a51a5f029ac44422ff9f8d0d361ed17db58d8
First Last <achraf.aaboul@gmail.com>
2020-06-04 03:26:31 +0100

    Added logic for accounts,lead and preinscription

--- force-app/main/default/lwc/lWC008_ShowCrechesLpcr/lWC008_ShowCrechesLpcr.js
+++ force-app/main/default/lwc/lWC008_ShowCrechesLpcr/lWC008_ShowCrechesLpcr.js
@@ -2,13 +2,19 @@ import { LightningElement, api, wire } from 'lwc';
 import getAccounts from '@salesforce/apex/APC007_ShowCrechesLpcrController.getAccounts';
 
 export default class LWC008_ShowCrechesLpcr extends LightningElement {
+    @api recordId;
     @api modeAffich;
     accounts;
+    center = {
+        Street: "Rue Barathon",
+        PostalCode: "20250"
+    };
     error;
     showFooter = true ;
-     @wire(getAccounts)
+    /*
+    @wire(getAccounts, {currentId : "$recordId", modeAffich: "$modeAffich"})
      wiredAccountsF({error,data}) {
-         if (data) {
+         if (data) {f
              this.accounts = data;
              console.log(data);
              console.log(JSON.stringify(data, null, '\t'));
@@ -16,5 +22,22 @@ export default class LWC008_ShowCrechesLpcr extends LightningElement {
              console.log(error);
              this.error = error;
          }
+     }*/
+     connectedCallback(){
+         console.log('gello');
+         console.log('recordId :' + this.recordId);
+         console.log('modAffich : ' + this.modeAffich);
+        getAccounts({currentId : this.recordId, modeAffich: this.modeAffich})
+        .then(res=>{
+            let jsonResult = JSON.parse(res);
+            this.accounts = jsonResult.mapMarkers;
+            this.center = jsonResult.center;
+            console.log('hii');
+            console.log(this.accounts);
+            console.log(this.center);
+        })
+        .catch(err=>{
+            console.log(err);
+        });
      }
 }
