e27b3c27551b4780fe780c6c4c3a5376f615c5b1
First Last <achraf.aaboul@gmail.com>
2020-06-23 14:46:48 +0100

    First commit of using Leaflet library

--- force-app/main/default/lwc/lWC008_ShowCrechesLpcr/lWC008_ShowCrechesLpcr.js
+++ force-app/main/default/lwc/lWC008_ShowCrechesLpcr/lWC008_ShowCrechesLpcr.js
@@ -1,23 +1,32 @@
-import { LightningElement, api, wire } from 'lwc';
-import getAccounts from '@salesforce/apex/APC007_ShowCrechesLpcrController.getAccounts';
+import { LightningElement, api, track } from 'lwc';
+import leaflet from '@salesforce/resourceUrl/Leaflet';
+import redmarker from '@salesforce/resourceUrl/redmarker';
+import getLocations from '@salesforce/apex/APC007_ShowCrechesLpcrController.getLocations';
 
 export default class LWC008_ShowCrechesLpcr extends LightningElement {
     @api recordId;
     @api modeAffich;
-    accounts;
-    center;
-    zoomLevel = 12;
-    showFooter = false;
-    listView = 'hidden';
-     connectedCallback(){
-        getAccounts({currentId : this.recordId, modeAffich: this.modeAffich})
-        .then(res=>{
-            let jsonResult = JSON.parse(res);
-            this.accounts = jsonResult.mapMarkers;
-            this.center = jsonResult.center;
-        })
-        .catch(err=>{
-            console.log(err);
-        });
-     }
+    showMap = false;
+    @track locations;
+    @track center;
+    icon;
+    initiated = false;
+    constructor(){
+        super();
+        this.icon = redmarker;
+    }
+    connectedCallback() {
+        getLocations({ currentId: this.recordId, modeAffich: this.modeAffich })
+            .then(result => {
+                console.log(result);
+                let jsonLocationsConfig = JSON.parse(result);
+                this.locations = jsonLocationsConfig.locations;
+                this.center = jsonLocationsConfig.center;
+                console.log('-->locations', this.locations);
+                this.showMap = true;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
 }
