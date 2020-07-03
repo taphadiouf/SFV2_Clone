9f627dc229eaae6b6e5d09bae5bbe43467f810a9
eyhuellou <60188887+eyhuellou@users.noreply.github.com>
2020-07-03 14:05:23 +0200

    add object field (#376)
    Co-authored-by: Franck <franckdepicardie@gmail.com>

--- force-app/main/default/lwc/lwc009_PsBroadcast/lwc009_PsBroadcast.js
+++ force-app/main/default/lwc/lwc009_PsBroadcast/lwc009_PsBroadcast.js
@@ -1,11 +1,15 @@
 import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import  getAllPermissionSet from '@salesforce/apex/APC009_PsBroadcastController.getAllPermissionSet';
+import  getAllObject from '@salesforce/apex/APC009_PsBroadcastController.getAllObject';
 import  executeAsyncPsUpdate from '@salesforce/apex/APC009_PsBroadcastController.executeAsyncPsUpdate';
 
 export default class Lwc009_PsBroadcast extends LightningElement {
 
     @track permissionSetlist;
+    @track objectlist;
     @track PsApiNameSelected;
+    @track ObjectApiNameSelected;
     @track error;
 
     settingCreate;
@@ -17,6 +21,7 @@ export default class Lwc009_PsBroadcast extends LightningElement {
         super();
     }
 
+
     @wire(getAllPermissionSet, {}) getAllPermissionSetFunction({error, data}) {
         if (data) {
             
@@ -25,6 +30,24 @@ export default class Lwc009_PsBroadcast extends LightningElement {
                 this.permissionSetlist.push({
                     label: data[i].Name,
                     value: data[i].Name
+                });
+            }
+            
+            this.error = undefined;
+        }
+        if (error) {
+            console.error(error);
+        }
+    }
+
+    @wire(getAllObject, {}) getAllObjectFunction({error, data}) {
+        if (data) {
+            
+            this.objectlist = [];
+            for (let i = 0; i < data.length; i++) {
+                this.objectlist.push({
+                    label: data[i],
+                    value: data[i]
                 });
             }
             
@@ -53,24 +76,47 @@ export default class Lwc009_PsBroadcast extends LightningElement {
     handlePsChosen(event){
         this.PsApiNameSelected = event.target.value;
     }
+    handleObjectChosen(event){
+        this.ObjectApiNameSelected = event.target.value;
+    }
+
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Error',
+            message: 'Please Choose a PS and an Object',
+            variant: 'error',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
+    }
 
     handelValidation(){
 
-        console.log('>>> HERE handleValidation - START');
-        console.log('>>> HERE handleValidation - this.settingRead:' + this.settingRead);
-        console.log('>>> HERE handleValidation - this.settingEdit:' + this.settingEdit);
-        console.log('>>> HERE handleValidation - this.PsApiNameSelected:' + this.PsApiNameSelected);
+        if (this.ObjectApiNameSelected == undefined || this.PsApiNameSelected == undefined) {
+            this.showToast();
+        }
+        else {
 
-        executeAsyncPsUpdate({ tmpCreate: this.settingCreate, tmpDelete: this.settingDelete, tmpRead: this.settingRead, tmpEdit: this.settingEdit, tmpPsName: this.PsApiNameSelected })
-            .then((result) => {
-                this.error = undefined;
-            })
-            .catch((error) => {
-                this.error = error;
-                console.log('>>> ERROR :' + this.error);
-            });
+        
 
-        console.log('>>> HERE handleValidation - FINISH');
+            console.log('>>> HERE handleValidation - START');
+            console.log('>>> HERE handleValidation - this.settingRead:' + this.settingRead);
+            console.log('>>> HERE handleValidation - this.settingEdit:' + this.settingEdit);
+            console.log('>>> HERE handleValidation - this.PsApiNameSelected:' + this.PsApiNameSelected);
+
+            executeAsyncPsUpdate({ tmpCreate: this.settingCreate, tmpDelete: this.settingDelete, tmpRead: this.settingRead, tmpEdit: this.settingEdit, tmpPsName: this.PsApiNameSelected, tmpObjectName: this.ObjectApiNameSelected })
+                .then((result) => {
+                    this.error = undefined;
+                })
+                .catch((error) => {
+                    this.error = error;
+                    console.log('>>> ERROR :' + this.error);
+                });
+
+            console.log('>>> HERE handleValidation - FINISH');
+
+        }
         
     }
 
