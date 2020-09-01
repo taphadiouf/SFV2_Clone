d0c1e725bf958a6c4106be6da1a358c2a8d0d9be
eyhuellou <60188887+eyhuellou@users.noreply.github.com>
2020-09-01 11:19:30 +0200

    Update scratchInit.sh
    changes for new adobe sign package version

--- scratchInit.sh
+++ scratchInit.sh
@@ -25,7 +25,7 @@ echo "----------- Sparklane :"
 sfdx force:package:install -p 04t4I000000gNw8QAE -u $YourAlias  -w 40
 
 echo "----------- Adobe Sign :"
-sfdx force:package:install -p 04t0g000000tOItAAM -u $YourAlias  -w 40
+sfdx force:package:install -p 04t0g000000xaQxAAI -u $YourAlias  -w 40
 
 echo "----------- CPQ - Conga Quotes :"
 sfdx force:package:install -p 04t5w000004cdSvAAI -u $YourAlias  -w 40 --noprompt
