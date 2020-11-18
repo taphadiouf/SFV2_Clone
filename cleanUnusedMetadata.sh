52359e12274a48d868c25d377be402c581a4085e
Franck <franckdepicardie@gmail.com>
2020-11-18 17:00:29 +0100

    cleaning APB 18/11

--- cleanUnusedMetadata.sh
+++
@@ -1,5 +0,0 @@
-echo "Start Cleaning..."
-#rm ./force-app/main/default/objects/blng__Invoice__c/fields/blng__InvoicePostedDate__c.field-meta.xml
-rm -rf ./force-app/main/default/flowDefinitions/*
-rm -rf ./mdapiout
-echo "Finished Cleaning."
